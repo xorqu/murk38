7043620bf155583cbcdb4758a10f01e0630a903a
xorqu <vobhod4@mail.ru>
2023-03-02 23:41:52 +0300

    texture filtering fix

--- levels/test_level/scripts/test_level.gd
+++ levels/test_level/scripts/test_level.gd
@@ -3,8 +3,12 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var mesh_instances = $Geometry.get_children()
+	print(mesh_instances)
+	for mesh_instance in mesh_instances:
+		for index in mesh_instance.mesh.get_surface_count():
+			var material = mesh_instance.mesh.surface_get_material(index)
+			material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS_ANISOTROPIC
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
