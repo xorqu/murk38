243676a54b39afb38873915d4e9df7f7f41779dd
xorqu <vobhod4@mail.ru>
2023-03-04 18:46:51 +0300

    edit main menu
    add levels list

--- ui/scripts/main_menu.gd
+++ ui/scripts/main_menu.gd
@@ -16,4 +16,14 @@ func _on_exit_button_pressed():
 
 
 func _on_start_button_pressed():
+	$VBoxContainer.set_visible(false)
+	$Levels_list.set_visible(true)
+
+
+func _on_test_level_button_pressed():
 	get_tree().change_scene_to_file("res://levels/test_level/scenes/test_level.tscn")
+
+
+func _on_levels_list_back_button_pressed():
+	$VBoxContainer.set_visible(true)
+	$Levels_list.set_visible(false)
