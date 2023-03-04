79c49a71688e27736c6ed99d8e13067649cc2def
xorqu <vobhod4@mail.ru>
2023-03-04 15:18:32 +0300

    add enemy navigation ai

--- levels/test_level/scripts/test_level.gd
+++ levels/test_level/scripts/test_level.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 
+@onready var player = $Player
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var mesh_instances = $Geometry.get_children()
+	var mesh_instances = $Geometry.get_child(0).get_children()
 	print(mesh_instances)
 	for mesh_instance in mesh_instances:
 		for index in mesh_instance.mesh.get_surface_count():
@@ -11,5 +13,5 @@ func _ready():
 			material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS_ANISOTROPIC
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	get_tree().call_group("enemy", "update_target_location", player.global_transform.origin )
--- mobs/test_enemy/test_enemy.gd
+++ mobs/test_enemy/test_enemy.gd
@@ -1,10 +1,20 @@
 extends CharacterBody3D
 
+var SPEED = 3.0
 var hp = 100
+@onready var nav_agent = $NavigationAgent3D
+
 
 func _physics_process(delta):
-	pass
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
+	velocity = velocity.move_toward(new_velocity, 0.25)
+	move_and_slide()
+	
+func update_target_location(target_location):
+	nav_agent.set_target_position(target_location)
 
 func get_damage(damage):
 	hp -= damage
