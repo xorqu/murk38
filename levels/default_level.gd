d24881dac0ed5b22ba272e8bd41e8cd68396d21a
xorqu <vobhod4@mail.ru>
2023-03-05 18:15:58 +0300

    create default_level

---
+++ levels/default_level.gd
@@ -0,0 +1,19 @@
+extends Node3D
+class_name default_level
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	texture_filtering_fix()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func texture_filtering_fix():
+	var mesh_instances = $NavigationRegion3D.get_child(0).get_children()
+	print(mesh_instances)
+	for mesh_instance in mesh_instances:
+		for index in mesh_instance.mesh.get_surface_count():
+			var material = mesh_instance.mesh.surface_get_material(index)
+			material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS_ANISOTROPIC
--- levels/test_level/scripts/test_level.gd
+++ levels/test_level/scripts/test_level.gd
@@ -1,20 +1,15 @@
-extends Node3D
+extends default_level
 
 
 @onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	texture_filtering_fix()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	get_tree().call_group("enemy", "update_target_location", player.global_transform.origin )
+	update_enemies_target()
+	
 
-func texture_filtering_fix():
-	var mesh_instances = $NavigationRegion3D.get_child(0).get_children()
-	print(mesh_instances)
-	for mesh_instance in mesh_instances:
-		for index in mesh_instance.mesh.get_surface_count():
-			var material = mesh_instance.mesh.surface_get_material(index)
-			material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS_ANISOTROPIC
+func update_enemies_target():
+	get_tree().call_group("enemy", "update_target_location", player.global_transform.origin)
