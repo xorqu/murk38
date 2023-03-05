ee12dcd0333f861328e1750c92d85e550f78198f
xorqu <vobhod4@mail.ru>
2023-03-05 18:40:26 +0300

    make enemy vars @export

--- mobs/default_enemy.gd
+++ mobs/default_enemy.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 class_name default_enemy
 
-var SPEED = 3.0
-var hp = 100
+@export var SPEED = 3.0
+@export var hp = 100
 @onready var nav_agent = $NavigationAgent3D
 
 func get_damage(damage):
