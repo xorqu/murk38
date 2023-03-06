befe41d1457ba10b1089f8e0569fd7702b533cc4
xorqu <vobhod4@mail.ru>
2023-03-06 14:29:43 +0300

    create ammo_item and weapon_item

--- items/weapon_item.gd
+++ items/weapon_item.gd
@@ -1,8 +1,7 @@
 extends default_item
-class_name medicine_item
+class_name weapon_item
 
-var heal_hp = 25
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -10,8 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _on_area_3d_body_entered(body):
-	if body.is_in_group("player"):
-		body.heal(heal_hp)
-	self.queue_free()
